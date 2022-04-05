4b0775f9bd230cb5dbc317561cc5c8994b14f7cf
Brant Martin <brantmartin473@gmail.com>
2022-04-04 23:42:28 -0700

    added function that adds score with initials and updated questions to be real

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,42 +4,45 @@ let button2 = document.getElementById('2')
 let button3 = document.getElementById('3')
 let button4 = document.getElementById('4')
 let header3 = document.getElementById('h3')
+let scoreBtn = document.getElementById('scoreBtn')
 let timer = document.getElementById('timer')
 let quizBtnVar = document.getElementById('quizBtn')
 let questionSec = document.getElementById('questions')
 let questionCont = document.getElementById('question-container')
+let displayResults = document.getElementById('displayResults')
 let questionNumber = 0
-let questionArr = [
-    {
-        q1:"Question1",
-        ch1:"option1",
-        ch2:"option2",
-        ch3:"option3",
-        ch4:"option4",
-        answer:"1"
-    },
-    {
-        q1:"Question2",
-        ch1:"option1",
-        ch2:"option2",
-        ch3:"option3",
-        ch4:"option4",
-        answer:"1"
-    },{
-        q1:"Question3",
-        ch1:"option1",
-        ch2:"option2",
-        ch3:"option3",
-        ch4:"option4",
-        answer:"3"
-    }
-]
 let score = 0
 let timerObj;
 let timerCounter = 30
+let liAppend = document.createElement('li');
+let questionArr = [
+    {
+        q:"Javascript is an _______ language?",
+        ch1:"Object-Oriented",
+        ch2:"Object-Based",
+        ch3:"Procedural",
+        ch4:"None of the above",
+        answer:"1"
+    },
+    {
+        q:"Which of the following keywords is used to define a variable in Javascript?",
+        ch1:"var",
+        ch2:"let",
+        ch3:"(A) and (B)",
+        ch4:"None of the above",
+        answer:"3"
+    },{
+        q:"What are var, let and const declare ________ in Javascript?",
+        ch1:"Syntax",
+        ch2:"Intervals",
+        ch3:"Attribute",
+        ch4:"Variables",
+        answer:"4"
+    }
+]
 
 questionCont.style.display = 'none'
-
+displayResults.style.display = 'none'
 quizBtnVar.addEventListener('click', startQuiz);
 
 button1.addEventListener('click', rightWrongFunc)
@@ -58,43 +61,54 @@ function startQuiz() {
        }
        else {
         timer.textContent = "Time's Up!"
-        showScore()
+        showScore();
        }
-   },1000)
-   displayQuestions()
-}
+   },1000);
+   displayQuestions();
+};
 
 
 
 function displayQuestions() {
-    question.textContent = questionArr[questionNumber].q1
-    button1.textContent = questionArr[questionNumber].ch1
-    button2.textContent = questionArr[questionNumber].ch2
-    button3.textContent = questionArr[questionNumber].ch3
-    button4.textContent = questionArr[questionNumber].ch4
+    question.textContent = questionArr[questionNumber].q;
+    button1.textContent = questionArr[questionNumber].ch1;
+    button2.textContent = questionArr[questionNumber].ch2;
+    button3.textContent = questionArr[questionNumber].ch3;
+    button4.textContent = questionArr[questionNumber].ch4;
 }
 
 function rightWrongFunc() {
-    var userAns = this.getAttribute('id')
+    var userAns = this.getAttribute('id');
     if ( userAns === questionArr[questionNumber].answer ){
         score += 10// score = score +10
-        header3.textContent = 'Correct'
+        header3.textContent = 'Correct';
     }
     else  {
+        timerCounter -= 10
         header3.textContent = 'Incorrect'
     }
      if (questionNumber < questionArr.length - 1) {
          questionNumber ++
-         displayQuestions()
+         displayQuestions();
      }
      else {
-         showScore()
+         showScore();
      }
         
 }
 
 function showScore() {
-    questionCont.style.display = 'none'
-    clearInterval(timerObj)
-
+    questionCont.style.display = 'none';
+    clearInterval(timerObj);
+    displayResults.style.display = 'block';
 }
+
+function addScore() {
+    let initInput = document.getElementById('initialsInp').value
+    let scoreWithInit = initInput + ' '  + score
+    liAppend.appendChild(document.createTextNode(scoreWithInit));
+ 
+    document.querySelector('ul').appendChild(liAppend);
+}
+
+scoreBtn.addEventListener('click', addScore)
